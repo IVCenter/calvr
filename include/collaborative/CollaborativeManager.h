a772106fd52056ea31f3cf7ed81bd9a9e9bf359b
ASP <aprudhom@coutsound.ucsd.edu>
2011-09-12 07:57:43 -0700

    AP doxygen updates

--- include/collaborative/CollaborativeManager.h
+++ include/collaborative/CollaborativeManager.h
@@ -1,3 +1,6 @@
+/**
+ * @file CollaborativeManager.h
+ */
 #ifndef CVR_COLLABORATIVE_MANAGER_H
 #define CVR_COLLABORATIVE_MANAGER_H
 
@@ -22,26 +25,40 @@ namespace cvr
         int numMes;
 };*/
 
-
+/**
+ * @brief Information passed for a client update
+ */
 struct ClientUpdate
 {
-    float objScale;
-    float objTrans[16];
-    int numMes;
+    float objScale; ///< object space scale
+    float objTrans[16]; ///< object space transform matrix
+    int numMes; ///< number of collaborative messages to follow
 };
 
+/**
+ * @brief Position/Rotation of a tracked body in a collaborative session
+ */
 struct BodyUpdate
 {
-        float pos[3];
-        float rot[4];
+        float pos[3]; ///< position
+        float rot[4]; ///< rotation (quat)
 };
 
+/**
+ * @brief Mode of the collaborative session
+ *
+ * LOCKED - all object space transforms synced to the session master
+ * UNLOCKED - everyone controls their own transforms and other clients hands/heads are visible
+ */
 enum CollabMode
 {
     LOCKED,
     UNLOCKED
 };
 
+/**
+ * @brief Type for a collaborative message
+ */
 enum CollaborativeMessageType
 {
     ADD_CLIENT = 0,
@@ -51,6 +68,9 @@ enum CollaborativeMessageType
     PLUGIN_MESSAGE
 };
 
+/**
+ * @brief Update sent from the collaborative server
+ */
 struct ServerUpdate
 {
         int numUsers;
@@ -60,95 +80,220 @@ struct ServerUpdate
 };
 
 // TODO: add version numbers for server/client interface
+/**
+ * @brief Session initialization info sent by the server
+ */
 struct ServerInitInfo
 {
-        int numUsers;
+        int numUsers; ///< number of clients in the session
 };
 
+/**
+ * @brief Header for all messages sent through the collaborative session
+ */
 struct CollaborativeMessageHeader
 {
-        int type;
-        int pluginMessageType;
-        char target[256];
-        int size;
-        bool deleteData;
+        int type; ///< CollaborativeMessageType
+        int pluginMessageType; ///< message type specified by a sending plugin
+        char target[256]; ///< name of target for this message
+        int size; ///< size of any message data
+        bool deleteData; ///< should the data for this message be deleted after sending
 };
 
+/**
+ * @brief Initialization information for a collaborative client
+ */
 struct ClientInitInfo
 {
         int id; ///< server fills this in
-        char name[256];
-        int numHeads;
-        int numHands;
+        char name[256]; ///< client name
+        int numHeads; ///< number of tracked heads
+        int numHands; ///< number of tracked hands
 };
 
 class CollaborativeThread;
 class CVRSocket;
 
+/**
+ * @brief Manages a collaborative session and handles collaborative message passing
+ */
 class CollaborativeManager
 {
     public:
         ~CollaborativeManager();
 
+        /**
+         * @brief Returns static self pointer
+         */
         static CollaborativeManager * instance();
 
+        /**
+         * @brief Called once at class creation
+         */
         bool init();
 
+        /**
+         * @brief Returns if connected to a collaborative session
+         */
         bool isConnected();
 
+        /**
+         * @brief Connect to a collaborative session
+         * @param host server host address/name
+         * @param port server port
+         */
         bool connect(std::string host, int port);
+
+        /**
+         * @brief Disconnect from a collaborative session
+         */
         void disconnect();
 
+        /**
+         * @brief Per frame update call
+         */
         void update();
 
+        /**
+         * @brief Returns this system's id number in the collaborative session
+         */
         int getID() { return _id; }
+
+        /**
+         * @brief Returns this system's collaborative name
+         */
         const std::string & getName() { return _myName; }
+
+        /**
+         * @brief Get the current mode of the collaborative session
+         */
         CollabMode getMode() { return _mode; }
+
+        /**
+         * @brief Set the mode of the collaborative session
+         */
         void setMode(CollabMode mode);
+
+        /**
+         * @brief Get the id number of the master client in the collaborative session
+         */
         int getMasterID() { return _masterID; }
+
+        /**
+         * @brief Set the id for the master client in the collaborative session
+         */
         void setMasterID(int id);
 
+        /**
+         * @brief Get the map of all clients in the collaborative session, indexed by id
+         */
         std::map<int,ClientInitInfo> & getClientInitMap() { return _clientInitMap; }
+
+        /**
+         * @brief Get the number of tracked heads for a client id
+         */
         int getClientNumHeads(int id);
+
+        /**
+         * @brief Get the number of tracked heads for a client id
+         */
         int getClientNumHands(int id);
+
+        /**
+         * @brief Get the head transform of a client
+         * @param id client id
+         * @param head tracked head number
+         */
         const osg::Matrix & getClientHeadMat(int id, int head);
+
+        /**
+         * @brief Get the hand transform of a client
+         * @param id client id
+         * @param hand tracked hand number
+         */
         const osg::Matrix & getClientHandMat(int id, int hand);
 
+        /**
+         * @brief Sends a message to a plugin in a collaborative session (asynchronous)
+         * @param target plugin to send message to
+         * @param type value the plugin gets as the message type
+         * @param data data to send for this message
+         * @param size size of data to send
+         * @param sendLocal if true, this message is also sent to the local instance of this plugin using the standard
+         *        message interface
+         *
+         * This call is asynchronous and the message will not likely be sent for at least a frame.  The data is assumed to be
+         * disposable and the buffer is deleted automatically after the message is sent.  If sendLocal is true, it happens before
+         * the function returns.
+         */
         void sendCollaborativeMessageAsync(std::string target, int type, char * data, int size, bool sendLocal = false);
+
+        /**
+         * @brief Sends a message to a plugin in a collaborative session (synchronous)
+         * @param target plugin to send message to
+         * @param type value the plugin gets as the message type
+         * @param data data to send for this message
+         * @param size size of data to send
+         * @param sendLocal if true, this message is also sent to the local instance of this plugin using the standard
+         *        message interface
+         *
+         * This is a synchronous call.  The function does not return until the message is sent.  The time taken depends on the 
+         * event in the collaborative queue and the network latency to the server.
+         */
         void sendCollaborativeMessageSync(std::string target, int type, char * data, int size, bool sendLocal = false);
 
     protected:
         CollaborativeManager();
+
+        /**
+         * @brief Update the collaborative tranforms
+         */
         void updateCollabNodes();
+
+        /**
+         * @brief Make geometry for a hand
+         */
         osg::Node * makeHand(int num);
+
+        /**
+         * @brief Make geometry for a head
+         */
         osg::Node * makeHead(int num);
+
+        /**
+         * @brief Start an update to the collaborative session
+         */
         void startUpdate();
+
+        /**
+         * @brief Handle a message received through the collaborative session
+         */
         void processMessage(CollaborativeMessageHeader & cmh, char * data);
 
-        static CollaborativeManager * _myPtr;
+        static CollaborativeManager * _myPtr; ///< static self pointer
 
-        std::string _myName;
+        std::string _myName; ///< name registered with collaborative session
 
-        cvr::CVRSocket * _socket;
+        cvr::CVRSocket * _socket; ///< collaborative session socket
 
-        cvr::CollaborativeThread * _thread;
+        cvr::CollaborativeThread * _thread; ///< thread that handles socket communication
 
-        std::map<int,ClientUpdate> _clientMap;
-        std::map<int,ClientInitInfo> _clientInitMap;
+        std::map<int,ClientUpdate> _clientMap; ///< map of all client object space transforms
+        std::map<int,ClientInitInfo> _clientInitMap; ///< map of all general client information
 
-        int _id;
-        bool _connected;
-        int _masterID;
-        CollabMode _mode;
+        int _id; ///< my collaborative id
+        bool _connected; ///< if connected to a collaborative session
+        int _masterID; ///< id of the current master client in the session
+        CollabMode _mode; ///< current collaborative mode
 
-        std::map<int,std::vector<BodyUpdate> > _handBodyMap;
-        std::map<int,std::vector<BodyUpdate> > _headBodyMap;
+        std::map<int,std::vector<BodyUpdate> > _handBodyMap; ///< map of all the client tracked hand body information
+        std::map<int,std::vector<BodyUpdate> > _headBodyMap; ///< map of all the client tracked head body information
 
-        osg::ref_ptr<osg::MatrixTransform> _collabRoot;
-        std::map<int,std::vector<osg::ref_ptr<osg::MatrixTransform> > > _collabHands;
-        std::map<int,std::vector<osg::ref_ptr<osg::MatrixTransform> > > _collabHeads;
+        osg::ref_ptr<osg::MatrixTransform> _collabRoot; ///< root of all collaborative geometry
+        std::map<int,std::vector<osg::ref_ptr<osg::MatrixTransform> > > _collabHands; ///< transforms for each hand geometry in the session
+        std::map<int,std::vector<osg::ref_ptr<osg::MatrixTransform> > > _collabHeads; ///< transforms for each head geometry in the session
 
-        std::queue<std::pair<CollaborativeMessageHeader,char*> > _messageQueue;
+        std::queue<std::pair<CollaborativeMessageHeader,char*> > _messageQueue; ///< queue of collaborative messages to send
 };
 
 }
--- include/config/ConfigManager.h
+++ include/config/ConfigManager.h
@@ -119,10 +119,28 @@ class CVRCONFIG_EXPORT ConfigManager
          */
         static float getFloat(std::string attribute, std::string path,
                               float def = 0.0, bool * found = NULL);
-
+        
+        /**
+         * @brief Looks for a double config file value in tag path with the default attribute
+         *        "value"
+         * @param path Tag to search for in the Tag1.Tag2.Tag3.Tag4 format, where Tag3
+         *             is the parent of Tag4 and Tag2 is the parent of Tag3 etc.
+         * @param def The default value to return if the tag is not found
+         * @param found If valid, *found is set to true if the tag existed and false
+         *              if the default value was returned
+         */
         static double getDouble(std::string path, double def = 0.0, bool * found =
                 NULL);
 
+        /**
+         * @brief Looks for a double config file value in tag path with the specified attribute
+         * @param attribute Attribute value within the tag to return
+         * @param path Tag to search for in the Tag1.Tag2.Tag3.Tag4 format, where Tag3
+         *             is the parent of Tag4 and Tag2 is the parent of Tag3 etc.
+         * @param def The default value to return if the tag is not found
+         * @param found If valid, *found is set to true if the tag existed and false
+         *              if the default value was returned
+         */
         static double getDouble(std::string attribute, std::string path,
                               double def = 0.0, bool * found = NULL);
 
@@ -173,36 +191,124 @@ class CVRCONFIG_EXPORT ConfigManager
         static bool getBool(std::string attribute, std::string path, bool def =
                 false, bool * found = NULL);
 
+        /**
+         * @brief Looks for a vector of floats in the tag path with default attributes "x","y","z"
+         * @param path Tag to search for in the Tag1.Tag2.Tag3.Tag4 format, where Tag3
+         *             is the parent of Tag4 and Tag2 is the parent of Tag3 etc.
+         * @param def The default value to return if the tag is not found
+         * @param found If valid, *found is set to true if the tag existed and false
+         *              if the default value was returned
+         */
         static osg::Vec3 getVec3(std::string path, osg::Vec3 def = osg::Vec3(0,0,0), 
                 bool * found = NULL);
 
+        /**
+         * @brief Looks for a vector of floats in the tag path with the specifed attributes for
+         *              each component
+         * @param attributeX attribute to read for x value
+         * @param attributeY attribute to read for y value
+         * @param attributeZ attribute to read for z value
+         * @param path Tag to search for in the Tag1.Tag2.Tag3.Tag4 format, where Tag3
+         *             is the parent of Tag4 and Tag2 is the parent of Tag3 etc.
+         * @param def The default value to return if the tag is not found
+         * @param found If valid, *found is set to true if the tag existed and false
+         *              if the default value was returned
+         */
         static osg::Vec3 getVec3(std::string attributeX, std::string attributeY, 
                 std::string attributeZ, std::string path, osg::Vec3 def = osg::Vec3(0,0,0),
                 bool * found = NULL);
 
+        /**
+         * @brief Looks for a vector of floats in the tag path with default attributes "x","y","z","w"
+         * @param path Tag to search for in the Tag1.Tag2.Tag3.Tag4 format, where Tag3
+         *             is the parent of Tag4 and Tag2 is the parent of Tag3 etc.
+         * @param def The default value to return if the tag is not found
+         * @param found If valid, *found is set to true if the tag existed and false
+         *              if the default value was returned
+         */
         static osg::Vec4 getVec4(std::string path, osg::Vec4 def = osg::Vec4(0,0,0,1), 
                 bool * found = NULL);
 
+        /**
+         * @brief Looks for a vector of floats in the tag path with the specifed attributes for
+         *              each component
+         * @param attributeX attribute to read for x value
+         * @param attributeY attribute to read for y value
+         * @param attributeZ attribute to read for z value
+         * @param attributeW attribute to read for w value
+         * @param path Tag to search for in the Tag1.Tag2.Tag3.Tag4 format, where Tag3
+         *             is the parent of Tag4 and Tag2 is the parent of Tag3 etc.
+         * @param def The default value to return if the tag is not found
+         * @param found If valid, *found is set to true if the tag existed and false
+         *              if the default value was returned
+         */
         static osg::Vec4 getVec4(std::string attributeX, std::string attributeY, 
                 std::string attributeZ, std::string attributeW, std::string path, 
                 osg::Vec4 def = osg::Vec4(0,0,0,1), bool * found = NULL);
 
-
+        /**
+         * @brief Looks for a vector of doubles in the tag path with default attributes "x","y","z"
+         * @param path Tag to search for in the Tag1.Tag2.Tag3.Tag4 format, where Tag3
+         *             is the parent of Tag4 and Tag2 is the parent of Tag3 etc.
+         * @param def The default value to return if the tag is not found
+         * @param found If valid, *found is set to true if the tag existed and false
+         *              if the default value was returned
+         */
         static osg::Vec3d getVec3d(std::string path, osg::Vec3d def = osg::Vec3d(0,0,0), 
                 bool * found = NULL);
 
+        /**
+         * @brief Looks for a vector of doubles in the tag path with the specifed attributes for
+         *              each component
+         * @param attributeX attribute to read for x value
+         * @param attributeY attribute to read for y value
+         * @param attributeZ attribute to read for z value
+         * @param path Tag to search for in the Tag1.Tag2.Tag3.Tag4 format, where Tag3
+         *             is the parent of Tag4 and Tag2 is the parent of Tag3 etc.
+         * @param def The default value to return if the tag is not found
+         * @param found If valid, *found is set to true if the tag existed and false
+         *              if the default value was returned
+         */
         static osg::Vec3d getVec3d(std::string attributeX, std::string attributeY, 
                 std::string attributeZ, std::string path, osg::Vec3d def = osg::Vec3d(0,0,0),
                 bool * found = NULL);
 
+        /**
+         * @brief Looks for a vector of doubles in the tag path with default attributes "x","y","z","w"
+         * @param path Tag to search for in the Tag1.Tag2.Tag3.Tag4 format, where Tag3
+         *             is the parent of Tag4 and Tag2 is the parent of Tag3 etc.
+         * @param def The default value to return if the tag is not found
+         * @param found If valid, *found is set to true if the tag existed and false
+         *              if the default value was returned
+         */
         static osg::Vec4d getVec4d(std::string path, osg::Vec4d def = osg::Vec4d(0,0,0,1), 
                 bool * found = NULL);
 
+        /**
+         * @brief Looks for a vector of floats in the tag path with the specifed attributes for
+         *              each component
+         * @param attributeX attribute to read for x value
+         * @param attributeY attribute to read for y value
+         * @param attributeZ attribute to read for z value
+         * @param attributeW attribute to read for w value
+         * @param path Tag to search for in the Tag1.Tag2.Tag3.Tag4 format, where Tag3
+         *             is the parent of Tag4 and Tag2 is the parent of Tag3 etc.
+         * @param def The default value to return if the tag is not found
+         * @param found If valid, *found is set to true if the tag existed and false
+         *              if the default value was returned
+         */
         static osg::Vec4d getVec4d(std::string attributeX, std::string attributeY, 
                 std::string attributeZ, std::string attributeW, std::string path, 
                 osg::Vec4d def = osg::Vec4d(0,0,0,1), bool * found = NULL);
 
-
+        /**
+         * @brief Looks for a color vector in the tag path with default attributes "r","g","b","a"
+         * @param path Tag to search for in the Tag1.Tag2.Tag3.Tag4 format, where Tag3
+         *             is the parent of Tag4 and Tag2 is the parent of Tag3 etc.
+         * @param def The default value to return if the tag is not found
+         * @param found If valid, *found is set to true if the tag existed and false
+         *              if the default value was returned
+         */
         static osg::Vec4 getColor(std::string path, osg::Vec4 def = osg::Vec4(1,1,1,1), 
                 bool * found = NULL);
 
