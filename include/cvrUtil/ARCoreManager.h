ffee3bca6d07c8d9e8a2ad47db58f9a3ded6ed56
ZhangMenghe <zmh.leaves@gmail.com>
2018-10-24 15:02:01 -0700

    update for plane

--- include/cvrUtil/ARCoreManager.h
+++ include/cvrUtil/ARCoreManager.h
@@ -4,14 +4,25 @@
 #include <cvrUtil/arcore_c_api.h>
 #include <osg/Matrixf>
 #include <GLES3/gl3.h>
+#include <unordered_map>
 
 namespace {
     const GLfloat kUVs[] = {
             0.0f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f,
     };
+    const uint32_t kPlaneRGBA[16] = {0xFFFFFFFF, 0xF44336FF, 0xE91E63FF, 0x9C27B0FF, 0x673AB7FF, 0x3F51B5FF,
+                                   0x2196F3FF, 0x03A9F4FF, 0x00BCD4FF, 0x009688FF, 0x4CAF50FF, 0x8BC34AFF,
+                                   0xCDDC39FF, 0xFFEB3BFF, 0xFFC107FF, 0xFF9800FF};
+    inline osg::Vec3f GetRandomPlaneColor(){
+        const int32_t colorRgba = kPlaneRGBA[std::rand() % 16];
+        return osg::Vec3f(((colorRgba >> 24) & 0xff) / 255.0f,
+                         ((colorRgba >> 16) & 0xff) / 255.0f,
+                         ((colorRgba >> 8) & 0xff) / 255.0f);
+    }
 }
 
 namespace cvr{
+    typedef std::unordered_map<ArPlane*, osg::Vec3f> planeMap;
     class ARCoreManager{
     private:
         static ARCoreManager* _myPtr;
@@ -33,6 +44,9 @@ namespace cvr{
         GLuint bgTextureId = 0;
         int32_t geometry_changed = 0;
 
+        /**Plane Factors***/
+        planeMap plane_color_map;
+
     public:
         static ARCoreManager * instance();
         ARCoreManager();
@@ -47,6 +61,11 @@ namespace cvr{
         void onDrawFrame();
 
         bool getPointCouldData(float*& pointCloudData, int32_t & point_num);
+
+        bool getPlaneData(ArPlane* plane, float*& plane_data,
+                          osg::Matrixf& modelMat, int32_t& vertice_num);
+
+        planeMap getPlaneMap();
 
         void setCameraTextureTarget(GLuint id){bgTextureId = id;}
 
--- src/cvrUtil/ARCoreManager.cpp
+++ src/cvrUtil/ARCoreManager.cpp
@@ -106,5 +106,85 @@ bool ARCoreManager::getPointCouldData(float*& pointCloudData, int32_t & point_nu
     memcpy(pointCloudData, _pointCloudData, memsize);
     return true;
 }
+bool ARCoreManager::getPlaneData(ArPlane* plane, float*& plane_data,
+                                 Matrixf& modelMat, int32_t& vertice_num){
+    int32_t polygon_length;
+    //get the number of elements(2*#vertives)
+    ArPlane_getPolygonSize(_ar_session, plane, &polygon_length);
+
+    if(polygon_length == 0){
+        LOGE("NO valid plane polygon found");
+        return false;
+    }
+    vertice_num = polygon_length/2;
+
+    plane_data = (float*)malloc(sizeof(float) * polygon_length);
+
+    ArPlane_getPolygon(_ar_session, plane, plane_data);
+
+    //get model matrix
+    ArPose * arPose;
+    ArPose_create(_ar_session, nullptr,&arPose);
+    ArPlane_getCenterPose(_ar_session, plane, arPose);
+    ArPose_getMatrix(_ar_session, arPose, modelMat.ptr());
+
+    return true;
+}
+//void ARCoreManager::doLightEstimation(){
+//    ArLightEstimate* ar_light_estimate;
+//    ArLightEstimateState ar_light_estimate_state;
+//    ArLightEstimate_create(_ar_session, &ar_light_estimate);
+//
+//    ArFrame_getLightEstimate(_ar_session, _ar_frame, ar_light_estimate);
+//    ArLightEstimate_getState(_ar_session, ar_light_estimate, &ar_light_estimate_state);
+//    if(ar_light_estimate_state == AR_LIGHT_ESTIMATE_STATE_VALID){
+//        ArLightEstimate_getColorCorrection(_ar_session, ar_light_estimate, _light.color_correction);
+//        ArLightEstimate_getPixelIntensity(_ar_session, ar_light_estimate, &_light.intensity);
+//    }
+//
+//    ArLightEstimate_destroy(ar_light_estimate);
+//}
+
+planeMap ARCoreManager::getPlaneMap(){
+    int detectedPlaneNum;
+    ArTrackableList* plane_list = nullptr;
+    ArTrackableList_create(_ar_session, & plane_list);
+    CHECK(plane_list!= nullptr);
+
+    ArSession_getAllTrackables(_ar_session, AR_TRACKABLE_PLANE, plane_list);
+    ArTrackableList_getSize(_ar_session, plane_list, &detectedPlaneNum);
+
+    for(int i=0; i<detectedPlaneNum; i++){
+        ArTrackable * ar_trackable = nullptr;
+        ArTrackableList_acquireItem(_ar_session, plane_list, i, &ar_trackable);
+
+        //cast down trackable to plane
+        ArPlane * ar_plane = ArAsPlane(ar_trackable);
+
+        //check the trackingstate, if not tracking, skip rendering
+        ArTrackingState trackingState;
+        ArTrackable_getTrackingState(_ar_session, ar_trackable, &trackingState);
+        if(trackingState != AR_TRACKING_STATE_TRACKING)
+            continue;
+
+        //check if the plane contain the subsume plane, if so, skip to avoid overlapping
+        ArPlane * subsume_plane;
+        ArPlane_acquireSubsumedBy(_ar_session, ar_plane, &subsume_plane);
+        if(subsume_plane != nullptr)
+            continue;
+
+        auto iter = plane_color_map.find(ar_plane);
+        if(iter == plane_color_map.end()){
+            if(plane_color_map.empty())
+                plane_color_map[ar_plane] = osg::Vec3f(1.0f, 1.0f, 1.0f);
+            else
+                plane_color_map[ar_plane] = GetRandomPlaneColor();
+        }
+        ArTrackable_release(ar_trackable);
+    }
+    ArTrackableList_destroy(plane_list);
+    plane_list = nullptr;
+    return plane_color_map;
+}
 osg::Matrixf ARCoreManager::getMVPMatrix(){Matrixf mat = (*view_mat) * (*  proj_mat);
     return mat;}
